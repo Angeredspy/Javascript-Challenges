decad82fbbbc30f886c5e63a1cecdec130c2f09f
--list <christo.graham1@gmail.com>
2022-09-09 10:03:01 -0400

    Corrects typo in problem description

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,5 +1,5 @@
 /*
-    Write a program that uses concole.log to print all the numbers
+    Write a program that uses console.log to print all the numbers
     from 1 to 100 with two exceptions- for numbers divisible by 3
     print 'Fizz'. For numbers divisible by 5, print 'Buzz' & 
     for numbers divisible by both 5 & 3 print 'FizzBuzz'! 
