3ba67fd76bc2572736d6d5833548033e1f18b335
--list <christo.graham1@gmail.com>
2022-09-09 10:14:11 -0400

    Initialize min function with two parameters

---
+++ Minimum.js
@@ -0,0 +1,14 @@
+/*
+    Write a function that takes two arguments 
+    & returns their minimum.
+*/   
+
+let min = (a, b) => {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+} 
+
+console.log(min(770,99));
