22163c92b7597c2e7c940c0314737b876bfacba7
--list <christo.graham1@gmail.com>
2022-09-09 08:54:12 -0400

    Initializes loop

---
+++ TriangleLoop.js
@@ -0,0 +1,9 @@
+/*
+    Write a loop that makes seven calls to console.log to output a trangle 
+    composed of pound characters.
+*/ 
+
+for (let x = '#'; x.length <= 7; x += '#'){
+    console.log(x);
+}; 
+
