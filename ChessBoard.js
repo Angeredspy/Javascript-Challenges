9edc4a27c9ea0d1f30e60e9c6492a25b13ad9ab1
--list <christo.graham1@gmail.com>
2022-09-09 10:04:54 -0400

    Initialize empty grid & max grid size.

---
+++ ChessBoard.js
@@ -0,0 +1,9 @@
+/*
+    Write a program which creates a string that is 
+    representative of an 8x8 grid, using newlines 
+    to separate each line. At each position of the 
+    grid, there is either a space or a '#' character.
+*/  
+
+let gridSize = 8; 
+let grid = '';
