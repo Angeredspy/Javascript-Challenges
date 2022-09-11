b34fdb46578f61beeae8316d6cb071bfaf0393af
--list <christo.graham1@gmail.com>
2022-09-11 11:32:57 -0400

    Adds challenge question

---
+++ DeepComparison.js
@@ -0,0 +1,7 @@
+/*
+    Write a function deepEqual that takes two values 
+    & returns true only if they are the same 
+    value, or if they are objects with the same props 
+    when evaluated via a recursive call. 
+*/ 
+
