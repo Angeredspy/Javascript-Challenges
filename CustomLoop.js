4216195044e3868cccd1b049f15ee7cb66783d78
--list <christo.graham1@gmail.com>
2022-09-12 16:10:00 -0400

    Defining the function & loop structure

---
+++ CustomLoop.js
@@ -0,0 +1,18 @@
+/* 
+    Construct a higher-order function 
+    which acts as a for loop. 
+    The function accepts a value, test function
+    update function & a body function. 
+    -- 
+    Every iteration, it first runs the test function on
+    the current loop value and stops if it returns false. 
+    It then calls the body function, passing the current value. 
+    Use the update function to create a new value & start 
+    from the beginning  
+*/ 
+
+let customLoop = (value, test, update, body) => {
+    for (let x = value; test(x); x = update(x)) {
+        body(x);
+    }
+}
