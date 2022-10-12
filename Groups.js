537d04049a705bd0b2c85e2e2339d2c7bf4f2d29
--list <christo.graham1@gmail.com>
2022-10-12 18:20:40 -0400

    Implements group class & associated methods

--- Groups.js
+++ Groups.js
@@ -1,11 +1,47 @@
 /* 
     Define a Class Group. Group has 
     add, delete, and has methods. 
-    Its constructor creates an empty group, add adds 
-    a value to the group, and delete removes its 
+    Its constructor creates an empty group. An Add method adds 
+    a value to the group, and a Delete method removes its 
     argument from the group and has returns a Boolean 
-    value indicating that  the argument is a member of the group. 
+    value indicating that the argument is a member of the group. 
     There is also a static (attached to the Class rather than instances of the Class) 
     'from' method that takes an iterable object, and creates a group 
     that has all the values produced from iterating over it.
 */ 
+
+class Group {
+    constructor () {
+        this.group = [];
+    }
+    add(value) {
+        if(!this.group.includes(value)) {
+            this.group.push(value);
+        }
+    }
+    delete(value){
+        let index = this.group.indexOf(value);
+        if (index !== -1) {
+            this.group.splice(index, 1); 
+        }
+    }
+    has(value) {
+        return this.group.includes(value)
+    }
+    static from(iterable){
+        let g = new Group();
+        for (let everything of iterable) {
+            g.add(everything)
+        }
+        return g;
+    }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
