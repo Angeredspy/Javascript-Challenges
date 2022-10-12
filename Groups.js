a1ec64d75593da158054726c721cc4e5c39d4e9c
--list <christo.graham1@gmail.com>
2022-10-12 18:00:36 -0400

    Adds base parameters

---
+++ Groups.js
@@ -0,0 +1,11 @@
+/* 
+    Define a Class Group. Group has 
+    add, delete, and has methods. 
+    Its constructor creates an empty group, add adds 
+    a value to the group, and delete removes its 
+    argument from the group and has returns a Boolean 
+    value indicating that  the argument is a member of the group. 
+    There is also a static (attached to the Class rather than instances of the Class) 
+    'from' method that takes an iterable object, and creates a group 
+    that has all the values produced from iterating over it.
+*/ 
